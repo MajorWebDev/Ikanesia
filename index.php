74977fad7a0d7c1172a686a8f579c1c66834c6e3
Mohammad Wildan Rizkiyadi <30789940+wildan68@users.noreply.github.com>
2021-11-18 12:15:28 +0700

    Add files via upload
    18 Nov

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
-    require_once "_config/config.php";
-    include 'header.html';
-    include 'content.html';
+include 'header.html';
 
-    if(isset($_SESSION['user'])) {
-        echo "<script>window.location='".base_url('dashboard')."';</script>";
-    } 
-?>
+if (isset($_GET['item'])) {
+    if ($_GET['item'] == 1) {
+        include 'item/index.php';
+    }
+    else {
+        include 'content.html';
+    }
+}
+else {
+    include 'content.html';
+}
+include 'footer.html';
