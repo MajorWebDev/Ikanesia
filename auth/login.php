fa9e6b35c3989e1bcc4d94017ba90d7eb8f0116e
Urip Raharjo <51041914+uripraharjo@users.noreply.github.com>
2022-01-13 11:35:37 +0700

    Update Style Login Page

--- auth/login.php
+++ auth/login.php
@@ -13,10 +13,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+	<link rel="stylesheet" href="../_asset/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../_asset/style.css">
     <title>Login - Ikanesia</title>
 </head>
 
-<body>
+<body class="text-center">
     <?php
     	if(isset($_POST['login'])) {
     		$user = trim(mysqli_real_escape_string($con, $_POST['user']));
@@ -31,12 +33,14 @@
     				}
     			}
     			?>
-    			<form action="" method="post">    				
-    					<input type="text" name="user" placeholder="Username" required autofocus>   
-    					<input type="password" name="pass" placeholder="Password" required>
-    					<input type="submit" name="login" value="Login"> 
+    			<form class="form-signin" action="" method="post">   
+					<img class="mb-3" src="../img/logo.png" width="72" height="72">
+					<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+					<input type="text" name="user" class="form-control" placeholder="Username" required autofocus> 
+    				<input type="password" name="pass" class="form-control" placeholder="Password" required>
+					<button class="btn btn-lg btn-primary btn-block mt-2" name="login" type="submit">Sign in</button>
+					<p class="mt-5 mb-3 text-muted">© 2021-2022 <a href="<?php echo base_url()?>">Ikanesia</a></p>
     			</form>
-	<a href="<?php echo base_url()?>">Kembali ke Beranda</a>
 </body>
 </html>
 <?php
