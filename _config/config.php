3818ae90a7157db7d791656e4c2a3878b0c03216
MajorWebDev <majorwebdev21@gmail.com>
2022-01-29 17:20:58 -0800

    fix config

--- _config/config.php
+++ _config/config.php
@@ -3,7 +3,7 @@
     date_default_timezone_set('Asia/Jakarta');
     session_start();
     //koneksi
-    $con = mysqli_connect('localhost','root','','ikanesia');
+    $con = mysqli_connect('be1x3hctw7lyrksp3fb3-mysql.services.clever-cloud.com','ur92hlofdmydyd8a','TbSxI9wsz3MvBEPUXHiZ','be1x3hctw7lyrksp3fb3');
     if(mysqli_connect_errno()) {
         echo mysqli_connect_error();
     }
